12b7028939798b0462e3cbbc09fb1366bdbd86a5
Gabriele-Zizzari-Peano-4A <56470876+Gabriele-Zizzari-Peano-4A@users.noreply.github.com>
2019-11-09 10:18:17 +0100

    Version 1.0
    versione 1.0
    con multi threading

---
+++ provaThread/src/provathread/ProvaThread.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provathread;
+
+/**
+ *
+ * @author zizzari.gabriele
+ */
+public class ProvaThread {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hi saluto = new Hi();
+        Hello saluto2 = new Hello();
+        saluto2.start();
+        saluto.start();
+    }
+
+}
